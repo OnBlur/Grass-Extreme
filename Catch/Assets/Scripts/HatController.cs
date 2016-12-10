01b33438e16f7652ae0306a8842c7c8dcf1b8ec1
Misha <michakiber1@gmail.com>
2016-12-10 11:49:38 +0100

    -Added movement to hat

---
+++ Catch/Assets/Scripts/HatController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatController : MonoBehaviour {
+
+    public Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		if(cam == null)
+        {
+            cam = Camera.main;
+        }
+	}
+	
+	// Update is called once per physics timestamp
+	void FixedUpdate () {
+        Vector3 rawPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 targetPosition = new Vector3(rawPosition.x, 0.0f, 0.0f);
+        GetComponent<Rigidbody2D>().MovePosition(targetPosition);
+    }
+}
