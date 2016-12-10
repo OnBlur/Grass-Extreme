540eff092b4ea60395507f455a708a3c77aabb8e
Misha <michakiber1@gmail.com>
2016-12-10 12:27:22 +0100

    -Finished part 1

--- Catch/Assets/Scripts/HatController.cs
+++ Catch/Assets/Scripts/HatController.cs
@@ -6,18 +6,27 @@ public class HatController : MonoBehaviour {
 
     public Camera cam;
 
+    private float maxWidth;
+
 	// Use this for initialization
 	void Start () {
 		if(cam == null)
         {
             cam = Camera.main;
         }
-	}
+        Vector2 upperCorner = new Vector2(Screen.width, Screen.height);
+        Vector2 targetWidth = cam.ScreenToWorldPoint(upperCorner);
+        float hatWidth = GetComponent<Renderer>().bounds.extents.x;
+        maxWidth = targetWidth.x - hatWidth;
+    }
 	
 	// Update is called once per physics timestamp
 	void FixedUpdate () {
-        Vector3 rawPosition = cam.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 targetPosition = new Vector3(rawPosition.x, 0.0f, 0.0f);
+        Vector2 rawPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 targetPosition = new Vector2(rawPosition.x, 0.0f);
+
+        float targetWidth = Mathf.Clamp(targetPosition.x, -maxWidth, maxWidth);
+        targetPosition = new Vector2(targetWidth, targetPosition.y);
         GetComponent<Rigidbody2D>().MovePosition(targetPosition);
     }
 }
