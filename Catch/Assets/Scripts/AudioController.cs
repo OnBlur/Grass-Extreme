b2e094c5d4701058181a6e03ddad6dc3a73381ea
Misha <michakiber1@gmail.com>
2016-12-15 19:21:02 +0100

    -Added Audio off icon if audio is switched off

--- Catch/Assets/Scripts/AudioController.cs
+++ Catch/Assets/Scripts/AudioController.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class AudioController : MonoBehaviour {
     
     public bool audioToggle;
+    public CanvasRenderer audioOn;
+    public CanvasRenderer audioOff;
+
 
     private void Start()
     {
         if (audioToggle = GetComponent<AudioSource>().gameObject.activeSelf)
         {
-            Debug.Log(audioToggle);
+            Debug.Log("Audio is: " + audioToggle);
+            audioOn.gameObject.SetActive(true);
         }
     }
 
@@ -18,12 +22,16 @@ public class AudioController : MonoBehaviour {
     {
         if (audioToggle)
         {
+            audioOn.gameObject.SetActive(false);
+            audioOff.gameObject.SetActive(true);
             AudioListener.pause = true;
             audioToggle = false;
             Debug.Log("Muziek is UIT");
         }
         else
         {
+            audioOff.gameObject.SetActive(false);
+            audioOn.gameObject.SetActive(true);
             AudioListener.pause = false;
             audioToggle = true;
             Debug.Log("Muziek is AAN");
