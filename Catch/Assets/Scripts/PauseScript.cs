bd94a1b2b164fc6c11bef9a74c944b177ccc6db2
Misha <michakiber1@gmail.com>
2017-01-04 17:20:56 +0100

    Version 0.6.6 -Added pause button and pause function while playing the game

---
+++ Catch/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseScript : MonoBehaviour
+{
+    public bool paused;
+    public Text pauseText;
+
+    // Use this for initialization
+    void Start()
+    {
+        paused = false;
+    }
+
+    public void Pause()
+    {
+        paused = !paused;
+
+        if (paused)
+        {
+            Time.timeScale = 0;
+            pauseText.gameObject.SetActive(true);
+        }
+        else if (!paused)
+        {
+            Time.timeScale = 1;
+            pauseText.gameObject.SetActive(false);
+        }
+    }
+}
